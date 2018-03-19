83b7fd4358aa6a29cbb95c74b6280d814fd9fdef
lelouche556 <gauravt5464@gmail.com>
2018-03-19 12:08:38 +0530

    modifications

--- site/animation.js
+++ site/animation.js
@@ -11,14 +11,12 @@ function showSlides() {
        slides[i].style.display = "none";  
     }
     slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
+    if (slideIndex > slides.length) {slideIndex = 1;}    
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex-1].style.display ="block";  
-    dots[slideIndex-1].className += "active";
-    var x;
-    x++;
-    console.log(x);
-    setTimeout(showSlides, 2000); 
+    dots[slideIndex-1].className += " active";
+    
+    setTimeout(showSlides, 4000); 
 }
